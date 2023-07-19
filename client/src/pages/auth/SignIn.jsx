9b76a10980327e3b7a71a53642cb5b44552c9d3a
Puskass <cosovictarik711@gmail.com>
2023-07-19 17:00:57 +0200

    making a profile working

--- client/src/pages/auth/SignIn.jsx
+++ client/src/pages/auth/SignIn.jsx
@@ -1,37 +1,33 @@
 import React, { useContext, useState } from "react";
+import axios from "axios";
 import { UserContext } from "../../context/UserContext";
 
 const LoginForm = () => {
   const { dispatch } = useContext(UserContext);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       // Make API request to login user
-      const response = await fetch("/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      });
-      const data = await response.json();
+      const response = await axios.post("http://localhost:5000/api/users/signin", formData);
       if (response.ok) {
         // User login successful
-        dispatch({ type: "LOGIN", payload: data.user });
+        console.log(response)
+        // dispatch({ type: "LOGIN", payload: data.user });
       } else {
         // Handle login error
-        console.error(data.error);
+        console.error(error);
       }
     } catch (error) {
       console.error(error);
@@ -43,14 +39,16 @@ const LoginForm = () => {
       <input
         type="text"
         placeholder="Username"
-        value={username}
-        onChange={handleUsernameChange}
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
       />
       <input
         type="password"
         placeholder="Password"
-        value={password}
-        onChange={handlePasswordChange}
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
       />
       <button type="submit">Login</button>
     </form>
