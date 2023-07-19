fdbb1f56d0913087b5abf0924328a00e103869d4
haraaaaaaa <harunhebibovic05@gmail.com>
2023-07-19 16:46:35 +0200

    more bugfixing

--- api/routes/auth/signup.js
+++ api/routes/auth/signup.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const { hash } = require("bcrypt");
-const { JWT_SECRET_KEY } = require("../../config/keys");
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY } = require("../../config/keys");
 
 const router = express.Router();
 router.post("/api/users/signup", async (req, res) => {
   const { username, firstName, lastName, email, password, confirmPassword } = req.body;
 
   const existingUser = await User.findOne({ username: username });
-  if (existingUser) return res.status(401).send({ message: "Auth failed" });
+  if (existingUser) return res.status(400).send({ message: "Username already in use" });
 
   if (password !== confirmPassword) return res.status(400).send({ message: "Passwords do not match" });
 
@@ -23,11 +24,12 @@ router.post("/api/users/signup", async (req, res) => {
     password: hashedPassword,
   });
 
-  res.send(createdUser, {
+  res.status(200).send({
     message: "Sign-Up Successful",
     token: jwt.sign({ id: createdUser._id }, JWT_SECRET_KEY, {
       expiresIn: "1h",
     }),
+    createdUser,
   });
 });
 
